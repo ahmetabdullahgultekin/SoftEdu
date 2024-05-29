fbb2d1bb78b796215186b42aa58ebe3beac60f41
Ahmet Abdullah Gültekin <ahabgu@gmail.com>
2024-05-29 11:44:37 +0300

    Last changes have made. Apk updated.

--- app/src/main/java/com/gultekinahmetabdullah/softedu/util/upload_apk_and_update_firestore.py
+++ app/src/main/java/com/gultekinahmetabdullah/softedu/util/upload_apk_and_update_firestore.py
@@ -1,18 +1,42 @@
-import hashlib
-import argparse
-
 import firebase_admin
-from firebase_admin import credentials, firestore, storage
+import hashlib
+import os
 import pyshorteners
+import pytz
+from datetime import datetime
+from firebase_admin import credentials, firestore, storage
+
+service_account_info = {
+    "type": "service_account",
+    "project_id": "quizapp-e69d7",
+    "private_key_id": "5fcd65d8db7603bda682fd369d3170012e031f13",
+    "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQDS7zJGLrYVbaJs\n2frKVuk99KupBMY0HAPHBBt6Mye07EddMV7Aur9ivblwImHeG57Hl2cxT5T2Hhhq\nQddk1m2/LYQqajuW6aerpuRrOwA+gaFYA1g6M1XDYTV6mC2ZliCW3YEXrMJsoqTu\ng0TYsd1SgmNS8sERekVI7YTN/ZUAWjhwJmluJ1jLyUU0g1INE4QLT7miNu86hxoh\nP7SUCz4hvACYBgLdaRbg2qStashzZ44d+3KQHxLem2ZhqrLi+VogWkhSrYZ31X4F\nRq0AYXgqD59cKnobkPy8qUCSCrg8ABZPCtxprWg3wMe5A3S4Ay2CUX0Noi3QbIBn\nDvvkdbZDAgMBAAECggEAAXBOOHpTFZxU0kc+Vaj4GEsV7KYyr+JNCEt34IJ+z9YY\nuoL+sXPUoFjI7gHeBwTLNXNL6eMwEemsovTmAjMQNpCZ6SwmUeYwFZSi1DRn0QVT\nU2VacbMt75I3KinljbmfaUgDfGTaQqFtXI8aphu4dbqb8NHlDTh3RoMQN9N187Wm\npUfINSBbm4/YDcxE/rwwvxE79At6VMHsbu4fMM5WcDRufF3vBvFI5cErKXTAQWji\nw0kImNDcaw14oIdATfxd6Alb3ST9ImGgJg39HRwq8eH5woTRKKOQk2maPUu6tGj2\n0pGrpgvtL3XHZwVNGfsaw7UZRxahNPoMGY103UORGQKBgQD5v7qgpSdp1cMTcSaP\now9MvefD9gdxqiAKFEJ9UCAlMAweTD+LRRCGq4ux64ot0Inn+RJgorysNXFBE2Y9\nmPMF6kYJMlp6xpEBDHTcwqaQk6D1kQodZ/gYop8SN6Xx0/uOvC4yH8eTYw+woi06\nnwzMtdGDotLwJxIkeSZDbtUIrwKBgQDYNsMhCNGyTNEnFjg5iNYfrHyQt2SJdMRN\n/KKos93JBvOj/IBfCiLSDAkgN1UHW8TsfTlRAuKNp4KGqor/LQE8f85u8gT/dCAa\npkVnYROkjVCDti24EtMvI6x02rKpsPDsozAn354cMqLZ7KBSj/erW8FFeuX64VUp\nMAZDNvSorQKBgAwNt9km/2b5PSgEDwNhsExY4gWwKx3OQyuZXLgllNeWoQ8ZSPvU\nwn5taxmUL6AUb4N1nvBxCkAZeDTW1dllN11yuBybhIhmavss+brp3hYeOeeAL4fJ\ny8yMlLbRXr30KDD0XhpnND+lyU0SFrK8mGaM4+bBhgK3MAX4nuMkRz3bAoGAO0Pn\ngKqWMDV2LyycBMlJSC1Th6d9W2AyXt4dB/kllfDd39laprJ0kPbTmY0RNY8mD1zT\n1SzhRMrhsGAW8ZM4f4jY4Phd/leqZSOmaovlgnlp1HCfX5FpacZxgyZfSrxBjdu9\nW3CkOj52/R0rZPYpgUMJWy4w0+/oE/4h+VKbBZECgYEAxSGgb20K0Tjx6e1hqpk8\nD69Ic2Us/vYTvu+SDKLNnlJO8ZiRBljCtEN9twyMy/6/+MBy6/VPMirrNzr4zeol\nFhatVE/vEwhPFVnQxcau1ZXSSJlzMCmgbgev5d2TVTFG7JSdbJTZPR+gkUQT0HTv\nERgtpCvmMD3+OZhPoH7CN3c=\n-----END PRIVATE KEY-----\n",
+    "client_email": "firebase-adminsdk-gzvp9@quizapp-e69d7.iam.gserviceaccount.com",
+    "client_id": "114312792834283930991",
+    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+    "token_uri": "https://oauth2.googleapis.com/token",
+    "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+    "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-gzvp9%40quizapp-e69d7.iam.gserviceaccount.com",
+    "universe_domain": "googleapis.com"
+}
 
 # Initialize the Firebase app with your service account credentials
-cred = credentials.Certificate("serviceAccountKey.json")
+cred = credentials.Certificate(service_account_info)
+
 firebase_admin.initialize_app(cred, {
     'storageBucket': 'quizapp-e69d7.appspot.com'
 })
 
 # Get a reference to the Firestore client
 db = firestore.client()
+
+
+def check_file_paths(file_paths):
+    for file_path in file_paths:
+        if os.path.isfile(file_path):
+            return file_path
+
+    return None
 
 
 def update_document(collection_name, document_id, updated_data):
@@ -47,15 +71,19 @@ def upload_apk_to_storage(local_file_path, destination_blob_name):
     # Create a new blob (file) in the bucket
     blob = bucket.blob(destination_blob_name)
 
+    # Compute the SHA256 hash code of the APK file
+    with open(local_file_path, "rb") as f:
+        bytes = f.read()  # read entire file as bytes
+        readable_hash = hashlib.sha256(bytes).hexdigest()
+
+    metadata = {"hashcode": readable_hash}
+    blob.metadata = metadata
+
     # Upload the local file to Firebase Storage
     blob.upload_from_filename(local_file_path)
     blob.make_public()
 
     print(f"File {local_file_path} uploaded to {destination_blob_name}.")
-    # Compute the SHA256 hash code of the APK file
-    with open(local_file_path, "rb") as f:
-        bytes = f.read()  # read entire file as bytes
-        readable_hash = hashlib.sha256(bytes).hexdigest()
 
     # Return the download URL and SHA256 hash code of the uploaded APK file
     return blob.public_url, readable_hash
@@ -69,32 +97,34 @@ def create_short_link(long_url):
         long_url (str): The long URL to shorten.
     """
     s = pyshorteners.Shortener()
-    short_url = s.tinyurl.short(long_url)
-    return short_url
+    return s.tinyurl.short(long_url)
 
 
 if __name__ == "__main__":
-    # Create the parser
-    parser = argparse.ArgumentParser(
-        description="Upload an APK file to Firebase Storage and update a Firestore document.")
+    # Get the current user's home directory
+    home_dir = os.path.expanduser("~")
 
-    # Add the arguments
-    parser.add_argument('local_file_path', type=str, help='The path to the local APK file.')
-    parser.add_argument('destination_blob_name', type=str, help='The name of the destination blob in Firebase Storage.')
-    parser.add_argument('document_id', type=str, help='The ID of the document to update.')
+    # Construct the file path
+    file_path = os.path.join(home_dir, "Documents/GitHub/SoftEdu/app/release/app-release.apk")
 
-    # Parse the arguments
-    args = parser.parse_args()
+    local_file_path = file_path
+    destination_blob_name = "SoftEdu.apk"
+    document_id = "latest"
 
     # Upload an APK file to Firebase Storage
-    download_url, hash_code = upload_apk_to_storage(args.local_file_path, args.destination_blob_name)
+    download_url, hash_code = upload_apk_to_storage(local_file_path, destination_blob_name)
     short_url = create_short_link(download_url)
+
+    # Get the current date and time in UTC+3
+    tz = pytz.timezone('Europe/Istanbul')  # Istanbul is in the UTC+3 timezone
+    current_time = datetime.now(tz)
 
     # Update a Firestore document
     updated_data = {
         "downloadLink": download_url,
         "shortLink": short_url,
         "hashCode": hash_code,
+        "timestamp": current_time
     }
     collection_name = "apks"
-    update_document(collection_name, args.document_id, updated_data)
+    update_document(collection_name, document_id, updated_data)
